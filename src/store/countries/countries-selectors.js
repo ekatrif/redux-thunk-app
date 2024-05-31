19632abf61ca930b32760af6931732ef45c5dd5f
Ekaterina Trifonova <ekaterinatrifonova@Ekaterinas-MacBook-Pro.local>
2024-05-31 09:43:34 +0300

    feat: Delete dev files

--- src/store/countries/countries-selectors.js
+++
@@ -1,9 +0,0 @@
-export const  selectCountriesInfo = (state) => ({
-  status: state.countries.status,
-  error: state.countries.error,
-  qty: state.countries.list.length,
-});
-
-export const selectAllCountries = (state) => state.countries.list;
-
-export const selectFilteredBySearchCountries = (state, { search='', region='' }) => state.countries.list.filter(c => c.name.common.toLowerCase().includes(search.toLowerCase()) && c.region.includes(region));
